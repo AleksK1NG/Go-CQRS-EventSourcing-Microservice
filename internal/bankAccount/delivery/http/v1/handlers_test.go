7436d3fd4db22b426efa62a419bbb54da545a64d
Alexander <alexander.bryksin@yandex.ru>
2022-07-08 09:10:36 +0300

    feature: add bank account http client tests update

--- internal/bankAccount/delivery/http/v1/handlers_test.go
+++ internal/bankAccount/delivery/http/v1/handlers_test.go
@@ -205,3 +205,99 @@ func TestBankAccountHandlers_ChangeEmail(t *testing.T) {
 	require.True(t, response.IsSuccess())
 	require.Equal(t, response.StatusCode(), http.StatusOK)
 }
+
+func TestBankAccountHandlers_GetByID(t *testing.T) {
+	t.Parallel()
+
+	client := http_client.NewHttpClient(true)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	command := commands.CreateBankAccountCommand{
+		Email:     gofakeit.Email(),
+		Address:   gofakeit.Address().Address,
+		FirstName: gofakeit.FirstName(),
+		LastName:  gofakeit.LastName(),
+		Status:    "PREMUIM",
+		Balance:   int64(gofakeit.Number(30000000, 5000000)),
+	}
+
+	response, err := client.R().
+		SetContext(ctx).
+		SetBody(command).
+		Post("http://localhost:5007/api/v1/accounts")
+	require.NoError(t, err)
+	require.NotNil(t, response)
+	require.False(t, response.IsError())
+	require.True(t, response.IsSuccess())
+	require.Equal(t, response.StatusCode(), http.StatusCreated)
+
+	var id string
+	err = json.Unmarshal(response.Body(), &id)
+	require.NoError(t, err)
+	require.NotEmpty(t, id)
+
+	t.Logf("response: %s", response.String())
+
+	response, err = client.R().
+		SetContext(ctx).
+		SetPathParam("id", id).
+		Get("http://localhost:5007/api/v1/accounts/{id}")
+
+	require.NoError(t, err)
+	require.NotNil(t, response)
+	require.False(t, response.IsError())
+	require.True(t, response.IsSuccess())
+	require.Equal(t, response.StatusCode(), http.StatusOK)
+	t.Logf("response: %s", response.String())
+}
+
+func TestBankAccountHandlers_Search(t *testing.T) {
+	t.Parallel()
+
+	client := http_client.NewHttpClient(true)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	command := commands.CreateBankAccountCommand{
+		Email:     gofakeit.Email(),
+		Address:   gofakeit.Address().Address,
+		FirstName: gofakeit.FirstName(),
+		LastName:  gofakeit.LastName(),
+		Status:    "PREMUIM",
+		Balance:   int64(gofakeit.Number(30000000, 5000000)),
+	}
+
+	response, err := client.R().
+		SetContext(ctx).
+		SetBody(command).
+		Post("http://localhost:5007/api/v1/accounts")
+	require.NoError(t, err)
+	require.NotNil(t, response)
+	require.False(t, response.IsError())
+	require.True(t, response.IsSuccess())
+	require.Equal(t, response.StatusCode(), http.StatusCreated)
+
+	var id string
+	err = json.Unmarshal(response.Body(), &id)
+	require.NoError(t, err)
+	require.NotEmpty(t, id)
+
+	t.Logf("response: %s", response.String())
+
+	response, err = client.R().
+		SetContext(ctx).
+		SetQueryParam("search", "a").
+		SetQueryParam("page", "1").
+		SetQueryParam("size", "10").
+		Get("http://localhost:5007/api/v1/accounts/search")
+
+	require.NoError(t, err)
+	require.NotNil(t, response)
+	require.False(t, response.IsError())
+	require.True(t, response.IsSuccess())
+	require.Equal(t, response.StatusCode(), http.StatusOK)
+	t.Logf("response: %s", response.String())
+}
